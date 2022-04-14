d5e0d74d94b583874645c5f8d8beb26c285224ab
kalina-ivanova28 <78103096+kalina-ivanova28@users.noreply.github.com>
2022-04-14 17:45:23 +0100

    Delete Working Prototype directory

--- Working Prototype/1 flower/Room_Bloom_Prototype/Room_Bloom_Prototype.ino
+++
@@ -1,9 +0,0 @@
-int piezoPin1 = 5; // Piezo on Analog Pin 3
-int sensorValue1 = 0; // A variable to store the value read from the sensor
-void setup() {
-  Serial.begin(9600);
-}
-void loop() {
-  sensorValue1 = analogRead(piezoPin1); // Read the value from the sensor
-  Serial.println(sensorValue1);
-}
